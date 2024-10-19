f97d11afc074476ac114ed0d9e95f60ab86a1601
NielsEngelhard <engelhardniels@gmail.com>
2024-10-19 19:51:39 +0200

    Initial commit

---
+++ TuringSolutions.WebFileUploadFacade/TuringSolutions.WebFileUploadFacade/FileUploadResult.cs
@@ -0,0 +1,6 @@
+﻿namespace TuringSolutions.WebFileUploadFacade;
+public class FileUploadResult
+{
+    public required Guid Id { get; set; }
+    public required string Url { get; set; }
+}
---
+++ TuringSolutions.WebFileUploadFacade/TuringSolutions.WebFileUploadFacade/IFileUploader.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace TuringSolutions.WebFileUploadFacade;
+public interface IFileUploader
+{
+    Task<FileUploadResult> UploadAndResizeIfNeeded(IFormFile file, string container, string path);
+    Task<FileUploadResult> UploadFile(IFormFile file, string container, string pathAndFileName);
+    Uri GetTempBlobLink(string container, string path, int expirationTimeInMinutes = 1);
+}
