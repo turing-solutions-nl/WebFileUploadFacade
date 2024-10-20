eb18b4d0abadc7fa67757424fefab85530ecb58f
NielsEngelhard <engelhardniels@gmail.com>
2024-10-20 17:20:31 +0200

    ImageResizer

---
+++ TuringSolutions.WebFileUploadFacade/TuringSolutions.WebFileUploadFacade/Resize/ImageResizer.cs
@@ -0,0 +1,19 @@
+﻿using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
+
+namespace TuringSolutions.WebFileUploadFacade.Resize;
+public static class ImageResizer
+{
+    public static byte[] ResizeImage(SixLabors.ImageSharp.Image image, int width, int height)
+    {
+        // Resize the image to the specified width and height
+        image.Mutate(x => x.Resize(width, height));
+
+        using var outputStream = new MemoryStream();
+
+        // Save the image as a JPEG format to the output stream
+        image.Save(outputStream, new JpegEncoder());
+
+        return outputStream.ToArray(); // Return resized image as byte array
+    }
+}
